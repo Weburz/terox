7e6e363e47fd33c35960a26b3cd422989ad5ea5f
Somraj Saha <somraj.saha@jarmos.dev>
2024-08-29 18:01:44 +0530

    chore: update the `version` command to print apt build info

--- cmd/version.go
+++ cmd/version.go
@@ -2,15 +2,20 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
+
+const version = "v0.0.1"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version information.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("RepoForge v0.0.1")
+		fmt.Print("RepoForge Build Information\n")
+		fmt.Printf("\nVersion: %s\n", version)
+		fmt.Printf("OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
 	},
 }
 
