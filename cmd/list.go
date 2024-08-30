db05e90995c03e4e718e4574d17b16c6bf2d43f5
Somraj Saha <somraj.saha@jarmos.dev>
2024-08-30 14:38:57 +0530

    chore: clean up and refactor the `list` command

--- cmd/list.go
+++ cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
@@ -12,20 +13,26 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all locally available templates.",
 	Run: func(cmd *cobra.Command, args []string) {
-		templates_dir := fmt.Sprintf("%s/repoforge", xdg.DataHome)
+		templates_dir := filepath.Join(xdg.DataHome, "repoforge")
 		templates, err := os.ReadDir(templates_dir)
 
+		// Throw error and exit execution if the data directory is unreadable
 		if err != nil {
-			fmt.Printf("No templates found at %s\n", templates_dir)
+			fmt.Printf("Failed to read %s directory: %v\n", templates_dir, err)
 			os.Exit(1)
 		}
 
 		fmt.Printf("Templates directory: %s\n\n", templates_dir)
-		fmt.Printf("Available templates:\n")
 
-		for _, template := range templates {
-			if template.IsDir() {
-				fmt.Printf("%s\n", template.Name())
+		// Share conditional message to user if no templates were found
+		if len(templates) == 0 {
+			fmt.Printf("Available templates: None\n")
+		} else {
+			fmt.Printf("Available templates:\n")
+			for _, template := range templates {
+				if template.IsDir() {
+					fmt.Printf("%s\n", template.Name())
+				}
 			}
 		}
 	},
