634f80dc0f11c6b675a5b3023b4f0beb83b86a58
Somraj Saha <somraj.saha@jarmos.dev>
2024-08-30 12:26:01 +0530

    chore: add implementation to list all available templates

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +12,22 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all locally available templates.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("TODO: All available templates")
+		templates_dir := fmt.Sprintf("%s/repoforge", xdg.DataHome)
+		templates, err := os.ReadDir(templates_dir)
+
+		if err != nil {
+			fmt.Printf("No templates found at %s\n", templates_dir)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Templates directory: %s\n\n", templates_dir)
+		fmt.Printf("Available templates:\n")
+
+		for _, template := range templates {
+			if template.IsDir() {
+				fmt.Printf("%s\n", template.Name())
+			}
+		}
 	},
 }
 
