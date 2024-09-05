9d009236e69252eaf38df37d16f665a5bf35f6e7
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-05 21:14:20 +0530

    chore: refactor the `generate` command a bit

--- cmd/generate.go
+++ cmd/generate.go
@@ -7,8 +7,18 @@ import (
 )
 
 var templateCmd = &cobra.Command{
-	Use:   "template",
-	Short: "Download a template from a remote source.",
+	Use:     "generate",
+	Short:   "Generate a template locally for future development needs.",
+	Aliases: []string{"gen"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 0 {
+			return fmt.Errorf(
+				"Command accepts 0 arguments but %d were passed",
+				len(args),
+			)
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Downloading templates...")
 	},
