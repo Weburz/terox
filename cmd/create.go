a17c765cc8237e95c4ff552af8b50d21efc98929
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-29 00:19:07 +0530

    chore: add basic logic to create the template in current working directory by default

--- cmd/create.go
+++ cmd/create.go
@@ -2,46 +2,42 @@ package cmd
 
 import (
 	"os"
-	"regexp"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
+// The location on disk to create the template to
+var location string
+
 var createCmd = &cobra.Command{
 	Use:     "create",
-	Short:   "Create a project using a template.",
-	Example: `forge create "Weburz/simple-website"`,
+	Short:   "Create a project template",
+	Example: `forge create "simple-website" --location="."`,
 	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		// Matches the pattern - "Weburz/repoforge" and so on
-		pattern := "^[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+$"
-
-		// Compiles the pattern into the binary before a string match can be
-		// performed
-		re, err := regexp.Compile(pattern)
-
-		// Throw and error and exit the execution if the pattern is invalid
-		if err != nil {
-			rootCmd.PrintErrf("Invalid regex pattern: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Match the accepted regex pattern with the argument passed to the
-		// command
-		match := re.MatchString(args[0])
-
-		// Throw error and exit the execution if the URL passed to the command
-		// is invalid
-		if !match {
-			rootCmd.PrintErrf("Error: %s is an invalid template URL\n", args[0])
-			os.Exit(1)
-		}
-
-		// TODO: Perform the rest of the logic if there were no errors
-		rootCmd.Printf("%s matches the pattern %s\n", args[0], pattern)
+		absLocation, _ := filepath.Abs(location)
+		createTemplate(args[0], absLocation)
 	},
 }
 
 func init() {
+	// Fetch the current working directory
+	cwd, _ := os.Getwd()
+	defaultLocation, _ := filepath.Abs(cwd)
+
+	// Add a flag called "--location" whose default value is the current working
+	// directory
+	createCmd.Flags().StringVar(
+		&location,
+		"location",
+		defaultLocation,
+		"specify the location to create the template at (e.g., ./templates)",
+	)
+
 	rootCmd.AddCommand(createCmd)
+}
+
+func createTemplate(name string, absLocation string) {
+	rootCmd.Printf("Creating the template \"%s\" at %s\n", name, absLocation)
 }
