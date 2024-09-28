3644fb375e7f956ffe3f38a4d9137fa1ec7fb215
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-29 00:26:04 +0530

    chore: refactor the code and add variables for legibility

--- cmd/create.go
+++ cmd/create.go
@@ -10,10 +10,24 @@ import (
 // The location on disk to create the template to
 var location string
 
+var shortUsage = "Create a project template"
+
+var longUsage = `
+Create a project template.
+
+Use this command to create a project template which can later be used to
+scaffold future projects! By default, the project is generated under the current
+working directory but it is possible to configure it by passing the "--location"
+flag.
+`
+
+var example = "forge create \"simple-website\" --location=\".\""
+
 var createCmd = &cobra.Command{
 	Use:     "create",
-	Short:   "Create a project template",
-	Example: `forge create "simple-website" --location="."`,
+	Short:   shortUsage,
+	Long:    longUsage,
+	Example: example,
 	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		absLocation, _ := filepath.Abs(location)
