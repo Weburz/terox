13dc3de9e1bca61e5454b4d43424a418c0684231
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-28 22:05:59 +0530

    chore: refactor the `clean` command for legibility

--- cmd/clean.go
+++ cmd/clean.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
 
@@ -14,51 +13,51 @@ var cleanCmd = &cobra.Command{
 	Aliases: []string{"gc", "cleanup"},
 	Short:   "Clean/delete all downloaded templates.",
 	Example: "forge clean\nforge gc\nforge cleanup",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) > 0 {
-			return errors.New("Command does not accept any arguments")
-		}
-		return nil
-	},
+	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		templatesDir := filepath.Join(xdg.DataHome, "repoforge")
-		templates, err := os.ReadDir(templatesDir)
-
-		// Throw error and exit execution loop if no templates were found
-		if err != nil {
-			rootCmd.Printf("Failed to find any templates at %s\n", templates)
-			os.Exit(1)
-		}
-
-		// Warn user and exit execution loop safely if no templates were found
-		if len(templates) == 0 {
-			rootCmd.Println("No templates were found.")
-			os.Exit(0)
-		}
-
-		rootCmd.Printf("The following templates were deleted:\n\n")
-
-		// Loop through the templates directory and delete everything
-		// (including files and folders) inside it
-		for _, template := range templates {
-			templatePath := filepath.Join(templatesDir, template.Name())
-
-			rootCmd.Printf("%s\n", template.Name())
-
-			err := os.RemoveAll(templatePath)
-
-			if err != nil {
-				rootCmd.Printf(
-					"Failed to delete %s: %v\n",
-					template.Name(),
-					err,
-				)
-				os.Exit(1)
-			}
-		}
+		cleanTemplates()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(cleanCmd)
+}
+
+func cleanTemplates() {
+	templatesDir := filepath.Join(xdg.DataHome, "repoforge")
+	templates, err := os.ReadDir(templatesDir)
+
+	// Throw error and exit execution loop if no templates were found
+	if err != nil {
+		rootCmd.Printf("Failed to find any templates at %s\n", templates)
+		os.Exit(1)
+	}
+
+	// Warn user and exit execution loop safely if no templates were found
+	if len(templates) == 0 {
+		rootCmd.Println("No templates were found.")
+		os.Exit(0)
+	}
+
+	// Print the location of the directory where the templates are stored at
+	rootCmd.Printf("The following templates were deleted:\n\n")
+
+	// Loop through the templates directory and delete everything
+	// (including files and folders) inside it
+	for _, template := range templates {
+		templatePath := filepath.Join(templatesDir, template.Name())
+
+		rootCmd.Printf("%s\n", template.Name())
+
+		err := os.RemoveAll(templatePath)
+
+		if err != nil {
+			rootCmd.Printf(
+				"Failed to delete %s: %v\n",
+				template.Name(),
+				err,
+			)
+			os.Exit(1)
+		}
+	}
 }
