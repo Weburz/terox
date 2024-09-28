b0f8685c96e618f47b1874014503ac25e66a8674
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-28 22:03:00 +0530

    chore: refactor the `list` command for legibility

--- cmd/list.go
+++ cmd/list.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
 
@@ -14,42 +13,42 @@ var listCmd = &cobra.Command{
 	Aliases: []string{"ls", "show"},
 	Short:   "List all locally available templates.",
 	Example: "forge list\nforge ls\nforge show",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) > 0 {
-			return errors.New("Command does not accept any arguments")
-		}
-		return nil
-	},
+	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		templates_dir := filepath.Join(xdg.DataHome, "repoforge")
-		templates, err := os.ReadDir(templates_dir)
-
-		// Throw error and exit execution if the data directory is unreadable
-		if err != nil {
-			rootCmd.PrintErrf(
-				"Failed to read %s directory: %v\n",
-				templates_dir,
-				err,
-			)
-			os.Exit(1)
-		}
-
-		rootCmd.Printf("Templates directory: %s\n\n", templates_dir)
-
-		// Share conditional message to user if no templates were found
-		if len(templates) == 0 {
-			rootCmd.Printf("Available templates: None\n")
-		} else {
-			rootCmd.Printf("Available templates:\n")
-			for _, template := range templates {
-				if template.IsDir() {
-					rootCmd.Printf("%s\n", template.Name())
-				}
-			}
-		}
+		listTemplates()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+}
+
+func listTemplates() {
+	templates_dir := filepath.Join(xdg.DataHome, "repoforge")
+	templates, err := os.ReadDir(templates_dir)
+
+	// Throw error and exit execution if the data directory is unreadable
+	if err != nil {
+		rootCmd.PrintErrf(
+			"Failed to read %s directory: %v\n",
+			templates_dir,
+			err,
+		)
+		os.Exit(1)
+	}
+
+	// Print the directory where the templates are located at
+	rootCmd.Printf("Templates directory: %s\n\n", templates_dir)
+
+	// Share conditional message to user if no templates were found
+	if len(templates) == 0 {
+		rootCmd.Printf("Available templates: None\n")
+	} else {
+		rootCmd.Printf("Available templates:\n")
+		for _, template := range templates {
+			if template.IsDir() {
+				rootCmd.Printf("%s\n", template.Name())
+			}
+		}
+	}
 }
