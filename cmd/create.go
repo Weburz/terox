688c79ba2356cb295ad7d67716a60643c8ce7376
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-29 01:23:26 +0530

    chore: implement logic to create necessary directories and a sample JSON config file

--- cmd/create.go
+++ cmd/create.go
@@ -7,8 +7,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// The location on disk to create the template to
-var location string
+// The templatePath on disk to create the template to
+var templatePath string
 
 var shortUsage = "Create a project template"
 
@@ -21,7 +21,7 @@ working directory but it is possible to configure it by passing the "--location"
 flag.
 `
 
-var example = "forge create \"simple-website\" --location=\".\""
+var example = "forge create \"simple-website\" --path=\".\""
 
 var createCmd = &cobra.Command{
 	Use:     "create",
@@ -30,28 +30,64 @@ var createCmd = &cobra.Command{
 	Example: example,
 	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		absLocation, _ := filepath.Abs(location)
-		createTemplate(args[0], absLocation)
+		// Get the template directory
+		templatePath, err := filepath.Abs(filepath.Join(templatePath, args[0]))
+
+		// Warn user and exit execution if an error was raised
+		if err != nil {
+			rootCmd.PrintErrf(
+				"Failed to identify template directory at %s\n",
+				templatePath,
+			)
+			os.Exit(1)
+		}
+
+		// Create the template
+		createTemplate(args[0], templatePath)
 	},
 }
 
 func init() {
 	// Fetch the current working directory
 	cwd, _ := os.Getwd()
-	defaultLocation, _ := filepath.Abs(cwd)
+
+	// The filepath where the template directory will be created by default
+	defaultPath, _ := filepath.Abs(cwd)
 
 	// Add a flag called "--location" whose default value is the current working
 	// directory
 	createCmd.Flags().StringVar(
-		&location,
-		"location",
-		defaultLocation,
-		"specify the location to create the template at (e.g., ./templates)",
+		&templatePath,
+		"path",
+		defaultPath,
+		"specify the path to create the template at (e.g., ./templates)",
 	)
 
 	rootCmd.AddCommand(createCmd)
 }
 
-func createTemplate(name string, absLocation string) {
-	rootCmd.Printf("Creating the template \"%s\" at %s\n", name, absLocation)
+func createTemplate(name string, templatePath string) {
+	rootCmd.Printf("Creating the template \"%s\" at %s\n", name, templatePath)
+
+	// Create the template directory
+	if err := os.MkdirAll(templatePath, os.ModePerm); err != nil {
+		rootCmd.PrintErrf(
+			"Failed to create  template directory at %s\n",
+			templatePath,
+		)
+		panic(err)
+	}
+
+	// Create the JSON "data" file to fetch some local information about the
+	// template from
+	if _, err := os.Create(
+		filepath.Join(templatePath, "forge.json"),
+	); err != nil {
+		rootCmd.PrintErrf(
+			"Failed to write the forge.json file to %s\n",
+			templatePath,
+		)
+		panic(err)
+	}
+
 }
