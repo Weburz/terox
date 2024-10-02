8afffd76bf485523b2cb4950b39e5e184a9ebe61
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-10-02 13:45:39 +0530

    ci: add a GH workflow to deploy the docs to GH pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
+  site: "https://weburz.github.io/repoforge",
+  base: "repoforge",
   integrations: [
     starlight({
       title: "RepoForge",
