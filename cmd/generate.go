708b91212cbace401b674c10904deb10a3f22ceb
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-10-27 03:14:42 +0530

    chore: refactor the command responsible for scaffolding the project

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,26 +1,56 @@
 package cmd
 
 import (
-	"fmt"
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
 )
 
+var generateCmdShortUsage = "Scaffold a project from a template"
+
+var generateCmdLongUsage = `
+Scaffold a project from a template.
+
+Use this command to scaffold a project from a template stored either locally or
+in a remote location (like a GitHub/GitLab repository). Support for other remote
+storage environments will be supported in a future version.
+`
+
+var generateCmdExample = "forge generate \"Weburz/nuxt-base\""
+
 var templateCmd = &cobra.Command{
 	Use:     "generate",
-	Short:   "Generate a template locally for future development needs.",
+	Short:   generateCmdShortUsage,
+	Long:    generateCmdLongUsage,
 	Aliases: []string{"gen"},
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) > 0 {
-			return fmt.Errorf(
-				"Command accepts 0 arguments but %d were passed",
-				len(args),
-			)
-		}
-		return nil
-	},
+	Example: generateCmdExample,
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Downloading templates...")
+		// Create a variable representation of the path to the template
+		templatePath, _ := filepath.Abs(
+			filepath.Join(
+				xdg.DataHome,
+				"repoforge",
+				// Represents the GitHub/GitLab user/organisation like "Weburz"
+				strings.Split(args[0], "/")[0],
+				// Represents the GitHub/GitLab repository like "nuxt-base"
+				strings.Split(args[0], "/")[1],
+			),
+		)
+
+		// Handle the logic to scaffold the project if the template is already
+		// available locally, else download it from the remote location
+		_, err := os.Stat(templatePath)
+		if errors.Is(err, os.ErrNotExist) {
+			rootCmd.Printf("No template named '%s' found locally\n", args[0])
+			rootCmd.Printf("Downloading template to %s\n", templatePath)
+		} else {
+			rootCmd.Printf("Scaffolding project from %s\n", templatePath)
+		}
 	},
 }
 
