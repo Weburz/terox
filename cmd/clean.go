42086ec4dc363eedc97a4afdec0766e9f18b0141
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-11-30 21:45:37 +0530

    chore: refactor the cleanup command

--- cmd/clean.go
+++ cmd/clean.go
@@ -7,74 +7,31 @@
 package cmd
 
 import (
-	"os"
-	"path/filepath"
-
-	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
+
+	"github.com/Weburz/terox/internal/template"
 )
+
+// Command help
+var cleanShortUsage = "Clean/delete all downloaded templates"
+var cleanLongUsage = `
+Cleanup the downloaded templates from disk.
+`
 
 // Command to handle the "clean" command of the CLI tool
 var cleanCmd = &cobra.Command{
 	Use:     "clean",
 	Aliases: []string{"gc", "cleanup"},
-	Short:   "Clean/delete all downloaded templates.",
+	Short:   cleanLongUsage,
+	Long:    cleanLongUsage,
 	Example: "terox clean\nterox gc\nterox cleanup",
 	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		cleanTemplates()
+		template.Clean()
 	},
 }
 
 // Register the command to the CLI tool
 func init() {
 	rootCmd.AddCommand(cleanCmd)
-}
-
-/**
- * cleanTemplates - Cleanup the template(s) from disk.
- *
- * Parameters:
- * None
- *
- * Returns:
- * None
- */
-func cleanTemplates() {
-	templatesDir := filepath.Join(xdg.DataHome, "terox")
-	templates, err := os.ReadDir(templatesDir)
-
-	// Throw error and exit execution loop if no templates were found
-	if err != nil {
-		rootCmd.Printf("Failed to find any templates at %s\n", templates)
-		os.Exit(1)
-	}
-
-	// Warn user and exit execution loop safely if no templates were found
-	if len(templates) == 0 {
-		rootCmd.Println("No templates were found.")
-		os.Exit(0)
-	}
-
-	// Print the location of the directory where the templates are stored at
-	rootCmd.Printf("The following templates were deleted:\n\n")
-
-	// Loop through the templates directory and delete everything
-	// (including files and folders) inside it
-	for _, template := range templates {
-		templatePath := filepath.Join(templatesDir, template.Name())
-
-		rootCmd.Printf("%s\n", template.Name())
-
-		err := os.RemoveAll(templatePath)
-
-		if err != nil {
-			rootCmd.Printf(
-				"Failed to delete %s: %v\n",
-				template.Name(),
-				err,
-			)
-			os.Exit(1)
-		}
-	}
 }
--- internal/template/template.go
+++ internal/template/template.go
@@ -44,3 +44,51 @@ func ListTemplates() error {
 
 	return nil
 }
+
+/**
+ * Clean: Cleanup the locally downloaded templates.
+ *
+ * Parameters:
+ *   None
+ *
+ * Returns:
+ *   None
+ */
+func Clean() error {
+	// Get the path to the template directory
+	templatesDir := filepath.Join(xdg.DataHome, "terox")
+
+	// Read the contents of the template directory to check for templates
+	templates, err := os.ReadDir(templatesDir)
+
+	// Throw an error if the template directory is unreachable
+	if err != nil {
+		return fmt.Errorf(
+			"Failed to find any templates at %s: %w",
+			templates,
+			err,
+		)
+	}
+
+	// Prompt the user about what template deletion
+	fmt.Printf("The following templates were deleted:\n\n")
+
+	// If templates were found locally, then attempt to delete them else throw
+	// an error
+	if len(templates) != 0 {
+		for _, template := range templates {
+			templatePath := filepath.Join(templatesDir, template.Name())
+			fmt.Printf("%s\n", template.Name())
+
+			if err := os.RemoveAll(templatePath); err != nil {
+				return fmt.Errorf(
+					"Failed to delete %s: %w",
+					template.Name(),
+					err,
+				)
+			}
+		}
+	}
+
+	return nil
+}
