4f906b2d6c9183d030e035a190c45e324c38e14e
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-12-08 22:35:39 +0530

    chore: refactor the `internal/template` package into multiple files

---
+++ internal/template/clean.go
@@ -0,0 +1,42 @@
+package template
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+/**
+ * Clean: Cleanup the locally downloaded templates.
+ *
+ * Parameters:
+ *   None
+ *
+ * Returns:
+ *   An error if any was raised during the removal process.
+ */
+func Clean() error {
+	// Read the contents of the template directory to check for templates
+	if templates, err := os.ReadDir(templateDir); err != nil {
+		return fmt.Errorf(
+			"Failed to find any templates at %s: %w",
+			templateDir,
+			err,
+		)
+	} else if len(templates) != 0 {
+		fmt.Printf("The following templates were deleted:\n\n")
+		for _, template := range templates {
+			path := filepath.Join(templateDir, template.Name())
+			fmt.Printf("%s\n", template.Name())
+			if err := os.RemoveAll(path); err != nil {
+				return fmt.Errorf(
+					"Failed to remove %s: %w",
+					template.Name(),
+					err,
+				)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ internal/template/list.go
@@ -0,0 +1,35 @@
+package template
+
+import (
+	"fmt"
+	"os"
+)
+
+/**
+ * List - List all the locally available templates.
+ *
+ * Parameters:
+ * None
+ *
+ * Returns:
+ * A wrapped error if any is raised.
+ */
+func List() error {
+	// Check if any template exists locally, if yes, list them to STDOUT
+	if templates, err := os.ReadDir(templateDir); err != nil {
+		return fmt.Errorf(
+			"Failed to read the contents of %s directory: %w",
+			templateDir,
+			err,
+		)
+	} else if len(templates) != 0 {
+		fmt.Printf("Available Templates:\n")
+		for _, template := range templates {
+			if template.IsDir() {
+				fmt.Printf("%s\n", template.Name())
+			}
+		}
+	}
+
+	return nil
+}
